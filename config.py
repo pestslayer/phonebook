7f20bfdd6b35e6d600d2afa47f821e31a3504e86
dennis mcmorris <dennis.mcmorris@icloud.com>
2023-11-30 21:32:38 -0600

    first commit

---
+++ config.py
@@ -0,0 +1,16 @@
+import os
+from dotenv import load_dotenv
+basedir = os.path.abspath(os.path.dirname(__file__))
+load_dotenv(os.path.join(basedir, '.env'))
+
+class Config():
+
+    """
+        Set config varibles for the flask app using Enviroment varibales where availible. Otherwise, create the config variables if not done already.
+    """
+
+    FLASK_APP = os.getenv('FLASK_APP')
+    FLASK_ENV = os.getenv('FLASK_ENV')
+    SECRET_KEY = os.environ.get('SECRET_KEY') or "You will not get access"
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URI') or 'sqlite:///' + os.path.join(basedir, 'app.db')
+    SQLALCHEMY_TRACK_NOTIFICATIONS = False
